9fec92b120ef3944eb0769872692774be4373978
desplega <desplega@desplega.com>
2022-11-16 10:07:09 +0100

    Project prepared

---
+++ tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CodeChallenge\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testMyTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
